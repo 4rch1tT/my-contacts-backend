6e0e97ecbafd1ad9725babdc803af65d6f7c20f2
Archit Anirudhan <architanirudhan123@gmail.com>
2026-01-06 12:56:06 +0530

    added error handling

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,6 +3,11 @@ const getContacts = (req, res) => {
 };
 
 const createContact = (req, res) => {
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are mandatory !");
+  }
   res.status(201).json({ message: "Create contact" });
 };
 
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 require("dotenv").config();
 const contactRouter = require("./routes/contactRoutes");
+const errorHandler = require("./middleware/errorHandler")
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use("/api/contacts", contactRouter);
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
