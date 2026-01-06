867abd15ee458b6ad6a1f620b860550ee97109d1
Archit Anirudhan <architanirudhan123@gmail.com>
2026-01-06 13:29:04 +0530

    estabilished mongo connection

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,27 +1,29 @@
-const getContacts = (req, res) => {
-  res.status(200).json({ message: "Get all contacts" });
-};
+const asyncHandler = require("express-async-handler");
 
-const createContact = (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Get all contacts" });
+});
+
+const createContact = asyncHandler(async (req, res) => {
   const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
     res.status(400);
     throw new Error("All fields are mandatory !");
   }
   res.status(201).json({ message: "Create contact" });
-};
+});
 
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Get contact for ${req.params.id}` });
-};
+});
 
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update contact for ${req.params.id}` });
-};
+});
 
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete contact for ${req.params.id}` });
-};
+});
 
 module.exports = {
   getContacts,
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 require("dotenv").config();
 const contactRouter = require("./routes/contactRoutes");
-const errorHandler = require("./middleware/errorHandler")
+const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
 
+connectDb();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json())
+app.use(express.json());
 app.use("/api/contacts", contactRouter);
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
